a759476c2a95e8b1c90f4b6d9a7a2ce753acc167
EmilianoAprile <emilianoaprile77@icloud.com>
2024-02-23 15:22:06 +0100

    added - drop down menu with jquery

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log ('collegato')
+
+// Drop Down Menu with jquery
+$(document).ready(function(){
+    $('#hamburger').click(function(){
+        $('#menu_drop').slideToggle(300)
+    })
+})
